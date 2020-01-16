00aa72b6ee585084b86960e3a6f41f4dbdf51cd9
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2020-01-16 13:17:53 -0300

    Working on tests

--- tests/testthat/test-common_workflow.R
+++ tests/testthat/test-common_workflow.R
@@ -10,12 +10,13 @@ test_that("Auto-Keras for images workflow", {
   c(x_train, y_train) %<-% mnist$train
   c(x_test, y_test) %<-% mnist$test
 
+  n <- 200
   # first 100 values for train
-  x_train <- x_train[1:100, , ]
-  y_train <- y_train[1:100]
+  x_train <- x_train[seq_len(n), , ]
+  y_train <- y_train[seq_len(n)]
   # and 20 for test
-  x_test <- x_test[1:20, , ]
-  y_test <- y_test[1:20]
+  x_test <- x_test[seq_len(n / 10), , ]
+  y_test <- y_test[seq_len(n / 10)]
 
   # as the input is the same, randomly get classifier or regressor
   if (sample(c(TRUE, FALSE), 1)) {
@@ -33,16 +34,4 @@ test_that("Auto-Keras for images workflow", {
   clf %>% evaluate(x_test, y_test)
   clf %>% predict(x_test)
 
-  # # get the Keras model to work with the Keras R library
-  # get_keras_model(clf)
-  #
-  # ak_file <- paste0(tempfile(), ".pkl")
-  # was_saved <- clf %>% export_autokeras_model(ak_file)
-  # if (was_saved) {
-  #   clf2 <- import_autokeras_model(ak_file)
-  #   expect_equal(
-  #     clf %>% predict(x_test),
-  #     clf2 %>% predict(x_test)
-  #   )
-  # }
 })
